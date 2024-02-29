91b12013d332d3d18095f10a15db50dc9052b70c
Seyves <igor.906090@gmail.com>
2024-02-29 12:20:39 +0300

    default shell fix

--- create-sym.sh
+++ create-sym.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/kitty ~/.config/kitty
+chsh -s $(which zsh)
