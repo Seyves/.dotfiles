e56ed951e57eb83b1e503076017486d5096d5b99
Seyves <igor.906090@gmail.com>
2024-02-29 19:12:29 +0300

    fonts and more plasma

--- debian-install.sh
+++ debian-install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 #dependencies
-sudo apt-get install tmux
-sudo apt-get install zsh
-sudo apt-get install zplug 
-sudo apt-get install kitty
-sudo apt-get install curl
-sudo apt-get install fzf
-sudo apt-get install fd-find
+sudo apt install tmux zsh zplug kitty curl wget fzf fd-find
 
 #nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
@@ -22,8 +16,18 @@ nvm install 20.11.1
 wget https://go.dev/dl/go1.22.0.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.tar.gz
 
+#fonst
+mkdir $HOME/.fonts
+cp -r ./fonts/* $HOME/.fonts/
+fc-cache -f -v
+
 #color-schemes
+mkdir $HOME/.local/share/color-schemes
 cp ./color-schemes/* $HOME/.local/share/color-schemes/
+plasma-apply-colorscheme BreezeGrayDark
+mkdir $HOME/.local/share/plasma/plasmoids
+cp -r ./plasma/org.kde.plasma.betterinlineclock $HOME/.local/share/plasma/plasmoids/
+cp -r ./plasma/org.kde.plasma.minimaldesktopindicator $HOME/.local/share/plasma/plasmoids/
 
 #symlinks
 ln -s $HOME/.dotfiles/tmux $HOME/.config/tmux     
@@ -32,3 +36,11 @@ ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
 ln -s $HOME/.dotfiles/zsh/.p10k.zsh $HOME/.p10k.zsh
 ln -s $HOME/.dotfiles/kitty $HOME/.config/kitty
 chsh -s $(which zsh)
+
+#lightly
+sudo apt install cmake build-essential libkf5config-dev libkdecorations2-dev libqt5x11extras5-dev qtdeclarative5-dev extra-cmake-modules libkf5guiaddons-dev libkf5configwidgets-dev libkf5windowsystem-dev libkf5coreaddons-dev libkf5iconthemes-dev gettext qt3d5-dev
+git clone --single-branch --depth=1 https://github.com/Luwx/Lightly.git
+cd Lightly && mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TESTING=OFF ..
+make
+sudo make install
