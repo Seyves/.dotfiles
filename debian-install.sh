8d3e69a59a2d46225e3d469fc6e656b571eb0351
Seyves <igor.906090@gmail.com>
2024-02-29 11:57:54 +0300

    zsh default terminal

--- debian-install.sh
+++ debian-install.sh
@@ -9,3 +9,4 @@ apt-get install fd-find
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 rm -rf /opt/nvim
 tar -C /opt -xzf nvim-linux64.tar.gz
+chsh -s $(which zsh)
