939849bc300e8dedb9d8f8976201fd176544669e
Seyves <igor.906090@gmail.com>
2024-03-08 02:38:25 +0300

    bug fix

--- nvim/after/plugin/colors-main.lua
+++ nvim/after/plugin/colors-main.lua
@@ -49,16 +49,3 @@ require("rose-pine").setup({
         italic = false,
     },
 })
-
-require("no-clown-fiesta").setup({
-    transparent = true, -- Enable this to disable the bg color
-    styles = {
-        -- You can set any of the style values specified for `:h nvim_set_hl`
-        comments = {},
-        keywords = {},
-        functions = {},
-        variables = {},
-        type = { bold = false },
-        lsp = { underline = true }
-    },
-})
