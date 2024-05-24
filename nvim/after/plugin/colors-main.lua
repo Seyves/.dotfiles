5cb37320caf414d3516e298f929328b16ce0c848
Seyves <igor.906090@gmail.com>
2024-05-25 01:20:26 +0300

    refactor

--- nvim/after/plugin/colors-main.lua
+++ nvim/after/plugin/colors-main.lua
@@ -14,49 +14,24 @@ require('colorizer').setup()
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
-local palette = {
-    bg1 = "#1b1b1b",
-    bg2 = "#242424",
-    bg3 = "#202020",
-    fg = "#c5c9c5"
-}
-
-require('kanagawa').setup({
-    commentStyle = { italic = true },
-    functionStyle = { bold = false },
-    booleans = { bold = false },
-    typeStyle = { bold = false },
-    keywordStyle = { bold = false, italic = false },
-    statementStyle = { bold = false },
-    transparent = true,
-    overrides = function()
-        return {
-            Boolean = { bold = false },
-            Todo = { bold = false },
-            ["@lsp.typemod.function.readonly"] = { bold = false, fg = palette.fg},
-            ["@keyword.operator"] = { bold = false },
-            TelescopeBorder = { fg = palette.bg3, bg = palette.bg3 },
-            TelescopeNormal = { fg = palette.fg, bg = palette.bg3 },
-            TelescopeTitle = { fg = "#8ba4b0" },
-            TelescopePromptNormal = { fg = palette.fg, bg = palette.bg2 },
-            TelescopePromptBorder = { fg = palette.bg2, bg = palette.bg2 },
-        }
+require('everforest').setup({
+    -- transparent_background_level = 2,
+    colours_override = function(palette)
+        palette.bg_dim = "#1b1b1b"
+        palette.bg0 = "#282828"
+        palette.bg1 = "#32302f"
+        palette.bg2 = "#32302f"
+        palette.bg3 = "#45403d"
+        palette.bg4 = "#45403d"
+        palette.bg5 = "#5a524c"
     end,
-    colors = {
-        palette = {
-            -- change all usages of these colors
-            dragonBlack0 = palette.bg3,
-            dragonBlack1 = palette.bg1,
-            dragonBlack2 = palette.bg1,
-            dragonBlack3 = palette.bg1,
-            dragonBlack4 = palette.bg1,
-            dragonBlack5 = palette.bg1,
-            --dragonYellow = palette.fg,
-            waveBlue1 = palette.bg2,
-            waveBlue2 = "#625E5A",
-            oldWhite = palette.fg,
-        },
-    }
+    on_highlights = function(hl, palette)
+        hl.TelescopeBorder = { fg = palette.bg1, bg = palette.bg1 }
+        hl.TelescopeNormal = { fg = palette.fg, bg = palette.bg1 }
+        hl.TelescopeTitle = { fg = palette.purple }
+        hl.TelescopePromptNormal = { fg = palette.fg, bg = palette.bg1 }
+        hl.TelescopePromptBorder = { fg = palette.bg1, bg = palette.bg1 }
+    end,
 })
 
 require("rose-pine").setup({
--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,28 +1,9 @@
 local lualine_settings = require("lualine-setting")
 
-require('everforest').setup({
-    -- transparent_background_level = 2,
-    on_highlights = function(hl, palette)
-        hl.TelescopeBorder = { fg = "#242424", bg = "#242424" }
-        hl.TelescopeNormal = { fg = palette.fg, bg = "#242424" }
-        hl.TelescopeTitle = { fg = palette.purple }
-        hl.TelescopePromptNormal = { fg = palette.fg, bg = palette.bg1 }
-        hl.TelescopePromptBorder = { fg = palette.bg1, bg = palette.bg1 }
-    end,
-    colours_override = function(palette)
-        palette.bg_dim = "#1b1b1b"
-        palette.bg0 = "#282828"
-        palette.bg1 = "#32302f"
-        palette.bg2 = "#32302f"
-        palette.bg3 = "#45403d"
-        palette.bg4 = "#45403d"
-        palette.bg5 = "#5a524c"
-    end,
-})
-
 vim.cmd.colorscheme("everforest")
 
 local custom_lualine_theme = require 'lualine.themes.everforest'
+
 custom_lualine_theme.normal.b.bg = "#282828"
 custom_lualine_theme.normal.c.bg = "#282828"
 custom_lualine_theme.insert.b.bg = "#282828"
@@ -31,6 +12,9 @@ custom_lualine_theme.visual.b.bg = "#282828"
 custom_lualine_theme.visual.c.bg = "#282828"
 custom_lualine_theme.command.b.bg = "#282828"
 custom_lualine_theme.command.c.bg = "#282828"
+custom_lualine_theme.inactive.b.bg = "#282828"
+custom_lualine_theme.inactive.c.bg = "#282828"
+
 lualine_settings.options.theme = custom_lualine_theme
 
 require('lualine').setup(lualine_settings)
